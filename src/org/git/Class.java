4b05cacaebc440f6ecb63da7ae437f27294ed18b
vishnuselvam95 <seleniumtraining95@gmail.com>
2023-01-31 15:26:59 +0530

    task1

---
+++ src/org/git/Class.java
@@ -0,0 +1,18 @@
+package org.git;
+
+public class Class {
+ private void javacourse() {
+System.out.println();
+
+}
+ 
+ private void pythoncourse() {
+System.out.println();
+}
+ 
+ public static void main(String[] args) {
+	Class c=new Class();
+	c.javacourse();
+	c.pythoncourse();
+}
+}
